2b6cee6aa32ac0c2f67c2945b5215732b311b165
Damian Ramirez <damianagustinrz@gmail.com>
2023-04-13 12:06:12 -0300

    fin ejercicio arbols

--- ejercicios/2023-f-pinos-damiramirez-main/src/Library.hs
+++ ejercicios/2023-f-pinos-damiramirez-main/src/Library.hs
@@ -3,3 +3,20 @@ module Library where
 type Number = Int
 
 pesoPino :: Number -> Number
+pesoPino altura 
+    | altura <= 300 = 3 * altura
+    | altura > 300 = 2 * altura
+    -- otherwhise?
+
+esPesoUtil :: Number -> Bool
+esPesoUtil peso = peso >= 400 && peso <= 1000
+
+sirvePino :: Number -> Bool
+sirvePino altura = pesoPino altura >= 400 && pesoPino altura <= 1000
+
+costoTransporte :: Number -> Number
+costoTransporte altura 
+    | pesoPino altura < 500 && sirvePino altura = 5000
+    | pesoPino altura >= 500 && pesoPino altura < 800 && sirvePino altura = 10 * pesoPino altura
+    | pesoPino altura >= 800 && sirvePino altura = (10 * pesoPino altura) + altura
+    | otherwise = 0 -- Iria una excepcion
